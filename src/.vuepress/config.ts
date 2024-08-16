8be7269eb2a31cf8f2ab892a9b6b2c9f75b868fb
Tsing <ld320321@163.com>
2024-08-16 17:49:21 +0800

    修改访问路径

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+  base: "/games-book/",
 
   lang: "zh-CN",
   title: "金色童年",
